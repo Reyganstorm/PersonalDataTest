4e4021c17b8ba878fe1a1aa0b7e985cc4ff43ab9
Reyganstorm <ruslan.shtybaev@gmail.com>
2022-10-24 14:54:35 +0300

    Initial Commit

---
+++ PersonalDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalDataTest
+//
+//  Created by Руслан Штыбаев on 24.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
